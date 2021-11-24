e8f90ca537893319910cb48ec49f364a1969fd4b
manimovassagh <mani.mgh@gmail.com>
2021-11-24 11:05:11 +0100

    add some components

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="table">
+    <table>
+      <template v-for="row in rows">
+        <tr
+          @click="toggle(row.id)"
+          :class="{ opened: opened.includes(row.id) }"
+          v-bind:key="row.id"
+        >
+          <td>{{ row.name }}</td>
+          <td>{{ row.handle }}</td>
+        </tr>
+      </template>
+    </table>
   </div>
 </template>
+
+
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+interface IRows{
+  id:number,
+  name:string,
+  handle:string
+}
+
+@Component({
+  components: {
+   
+  },
+})
+export default class Home extends Vue {
+ 
+opened:[]= []
+
+  rows:IRows[]= [
+        { id: 1, name: "Bill", handle: "bill" },
+        { id: 2, name: "Bob", handle: "bob" },
+        { id: 3, name: "Jim", handle: "jim" },
+        { id: 4, name: "Leroy", handle: "leroy" },
+      ]
+
+   toggle(id:number):void {
+     
+      const index = this.opened.indexOf(id);
+      if (index > -1) {
+        this.opened.splice(index, 1);
+      } else {
+        this.opened.push(id);
+      }
+}
+}
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border: 1px solid #ccc;
+}
+
+td {
+  padding: 2px;
+  border: 1px solid #ccc;
+}
+
+.opened {
+  background-color: yellow;
+}
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="table">
+   <h1>check</h1>
+  </div>
+</template>
+
+
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+   
+  },
+})
+export default class Contact extends Vue {
+ 
+
+
+   
+}
+
+</script>
+
+<style scoped>
+
+</style>
