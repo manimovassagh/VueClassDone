35d229a4a2b237dee37ee01a0e3971d354b544bd
manimovassagh <mani.mgh@gmail.com>
2021-11-24 11:36:00 +0100

    1

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="table">
    <h1>check</h1>
+   <div >{{fullName()}}</div>
   </div>
 </template>
 
@@ -17,8 +18,12 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class Contact extends Vue {
  
+ name='mani'
+ family='Movassagh'
 
-
+fullName():string{
+   return ` This is a ${this.name} and this is a ${this.family}`
+  }
    
 }
 
