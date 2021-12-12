031efc56ba59b14963159d95ec5f271a7ce648b5
Mani Movassagh <mani.mgh@gmail.com>
2021-12-12 01:03:53 +0100

    111

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+ <h1>second</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+
+
+@Component({
+  components: {
+   
+  },
+})
+export default class Footer extends Vue {
+  
+}
+</script>
